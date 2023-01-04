0475b4f7ce66713019298af0a095bad570be15ec
whoiam <119673245+lprlpr666@users.noreply.github.com>
2023-01-04 22:25:19 +0800

    Add files via upload

--- cmake-build-debug-coverage/cmake_install.cmake
+++ cmake-build-debug-coverage/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/陆鹏睿/Downloads/Compressed/jjj/Bookstore-main
+# Install script for directory: /mnt/d/周秉霖/coding/Bookstore
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,11 +37,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -50,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/陆鹏睿/Downloads/Compressed/jjj/Bookstore-main/cmake-build-debug-coverage/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/d/周秉霖/coding/Bookstore/cmake-build-debug-coverage/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
