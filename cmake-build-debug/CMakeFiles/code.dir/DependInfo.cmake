0f77b3b1b8f1e3ef4b0d502aa65646e2e8b41c85
whoiam <119673245+lprlpr666@users.noreply.github.com>
2023-01-04 23:05:31 +0800

    Add files via upload

--- cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/c/Users/陆鹏睿/Downloads/Compressed/jjj/Bookstore-main/main.cpp" "CMakeFiles/code.dir/main.cpp.o" "gcc" "CMakeFiles/code.dir/main.cpp.o.d"
+  "/mnt/c/Users/陆鹏睿/Downloads/Compressed/okk/bookstore-main/main.cpp" "CMakeFiles/code.dir/main.cpp.o" "gcc" "CMakeFiles/code.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
